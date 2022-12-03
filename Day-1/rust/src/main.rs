e1b07572910ea3af79b362c9be84d5eb210a91e9
Michael Pacheco <git@michaelpacheco.org>
2022-12-02 20:51:01 -0800

    completed optimized rust part 1.

--- Day-1/rust/src/main.rs
+++ Day-1/rust/src/main.rs
@@ -1,54 +1,38 @@
 use std::fs::File;
 use std::io::{BufRead, BufReader};
+use std::time::{Duration, Instant};
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let start: Instant = Instant::now();
     let path = "../input.txt";
     let input = File::open(path)?;
+
     let buf = BufReader::new(input);
-    let mut elf = 0;
-    let mut calories_total: i32 = 0;
-    let mut elf_max: i32 = 0;
-    let mut items: i32 = 0;
-    let mut lines: i32 = 0;
-    let mut calories: Vec<i32> = Vec::with_capacity(260);
-    for line in buf.lines(){
-        lines += 1;
-        let unwrap = line?;
-        println!("Elf #{elf}  with {items} item(s) that has {unwrap} calories with a total of {calories_total}, line {lines}");
-        if unwrap.len() == 0 {
-            if elf != 0 {
-                if calories_total > calories[elf_max as usize] {
-                    elf_max = elf;
-                }
+
+    let mut cur_cal = 0;
+    let mut max_cal: i32 = 0;
+
+    let debug = false;
+
+
+    let mut line_handler = |line: String| {
+        if line.len() == 0 {
+            if debug {println!("CURRENT: CAL {cur_cal}" )};
+            if cur_cal > max_cal {
+                max_cal = cur_cal
             }
-            calories.push(calories_total);
-            println!("Elf #{elf} brought {items} items with a total of {calories_total} calories");
-            calories_total = 0;
-            items = 0;
-            elf += 1;
+            cur_cal = 0;
         } else {
-            items += 1;
-            calories_total += unwrap.parse::<i32>()?;
+            cur_cal += line.parse().unwrap_or(0);
         }
+    };
+
+    for line in buf.lines() {
+        line_handler(line?);
     }
-    let cal = calories[elf_max as usize];
-    elf_max += 1;
-    let cal_len = calories.len();
-    println!("The elf with that brought the most calories is {elf_max} with {cal} calories and {cal_len} elves, total of {lines} lines");
+    println!("The answer for Day 1 part 1 is {max_cal}");
+    let duration: Duration  = start.elapsed();
+    println!("Time elapsed is: {duration:?}");
     Ok(())
 }
 
-enum ElfCond {
-    Done,
-
-}
-
-
-
-fn getMaxElf(elf: i32, value: String, store: i32) -> (i32, i32) {
-    if value.len() == 0 {
-        (elf, value)
-    }
-    let ret = store + unwrap.parse::<i32>()?;
-    (elf, ret)
-}
