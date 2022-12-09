97c0817c52c935b6d2458a3f67a93ea8b8be9385
Michael Pacheco <git@michaelpacheco.org>
2022-12-08 21:38:00 -0800

    Extract portions of part1's code so that they can be reused.
    Signed-off-by: Michael Pacheco <git@michaelpacheco.org>

--- Day-2/rust/src/main.rs
+++ Day-2/rust/src/main.rs
@@ -1,49 +1,88 @@
-use std::{fs::File,
-          io::{BufRead, BufReader},
-          time::Instant,
-          collections::HashMap
+use std::{
+    collections::HashMap,
+    fs::File,
+    io::{BufRead, BufReader},
+    time::Instant,
 };
 
+// Result<(), Box<dyn std::error::Error>>
 
-
-// The score for a single round is the score for the shape you selected
-// (1 for Rock, 2 for Paper, and 3 for Scissors)
-// plus the score for the outcome of the round
-// (0 if you lost, 3 if the round was a draw, and 6 if you won).
-
-
-fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let mut outcomes = HashMap::new();
-
+fn main() {
     let start: Instant = Instant::now();
-    let path = "outcomes.txt";
-    let input = File::open(path)?;
-    let buf = BufReader::new(input);
-    for line in buf.lines() {
+    let outcomes = outcomes();
+    let p1_score = part_1(&outcomes);
+    let p1_duration = start.elapsed();
+    println!("The score for the player using the guide is {p1_score}");
+    println!("Time elapsed {p1_duration:?}");
+}
+
+fn reader(path: &str) -> BufReader<File> {
+    // I am fine with it panic'ing here, all of the code depends on these lines
+    // TODO: might figure out later a better way to refactor this code.
+    let input = File::open(path).unwrap();
+    return BufReader::new(input);
+}
+
+fn outcomes() -> HashMap<String, i32> {
+    let mut outcomes = HashMap::new();
+    for line in reader("outcomes.txt").lines() {
         let line = line.unwrap_or(String::from("//"));
-        if line.contains("//") {
+        // ignore lines that start with //
+        if line.contains("//") || line.is_empty() {
             continue;
         }
         // TODO: Need to figure out if i can simplify this ugly code
-        let outcome = line.split(":").map(|i| String::from(i)).collect::<Vec<String>>();
-        outcomes.insert(outcome[0].clone(), i32::from_str_radix(outcome[1].clone().as_str(), 10).unwrap_or(0));
+        let outcome = line
+            .split(":")
+            .map(|i| String::from(i))
+            .collect::<Vec<String>>();
+        outcomes.insert(
+            outcome[0].clone(),
+            i32::from_str_radix(outcome[1].clone().as_str(), 10).unwrap_or(0),
+        );
     }
-    let path = "../input.txt";
-    let input = File::open(path)?;
+    outcomes
+}
 
-    let buf = BufReader::new(input);
+fn part_1(choices: &HashMap<String, i32>) -> i32 {
+    // The score for a single round is the score for the shape you selected
+    // (1 for Rock, 2 for Paper, and 3 for Scissors)
+    // plus the score for the outcome of the round
+    // (0 if you lost, 3 if the round was a draw, and 6 if you won).
+    // already precompiled all outcomes
+
     let mut score: i32 = 0;
 
-    for line in buf.lines() {
+    for line in reader("../input.txt").lines() {
         let line = line.unwrap_or(String::from(""));
         // let outcome = line.split(" ").collect::<Vec<&str>>();
         if line.is_empty() {
             continue;
         }
-        score += outcomes[line.as_str()];
+        score += outcomes[&line];
     }
-    let duration = start.elapsed();
-    println!("The score for the player using the guide is {score}");
-    println!("Time elapsed {duration:?}");
-    Ok(())
+    score
+}
+
+fn part_2() {
+    // X means you need to lose, Y means you need to end the round in a draw, and Z means you need to win.
+    let mut choice: HashMap<String, String> = HashMap::new();
+    for line in reader("part2.txt") {
+        // TODO: abstract this code, maybe return a Hashmap in this case.
+        let outcome = line
+            .split(":")
+            .map(|i| String::from(i))
+            .collect::<Vec<String>>();
+        choice.insert(outcome[0].clone(), outcome[1].clone());
+    }
+    for line in reader("../input.txt").lines() {
+        let line = line.unwrap_or(String::from(""));
+        // let outcome = line.split(" ").collect::<Vec<&str>>();
+        if line.is_empty() {
+            continue;
+        }
+        let enemy = line.split(" ").collect::<Vec<&str>>()[0];
+        let p2_choice = choice[&line].as_str();
+        let integrate = format!("{enemy} {p2_choice}");
+    }
 }
