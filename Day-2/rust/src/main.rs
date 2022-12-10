7dd48b2e5448025949d40cdfc5baeb2b2ea0be0c
Michael Pacheco <git@michaelpacheco.org>
2022-12-09 17:18:31 -0800

    Change code to be more declaritive, for more reable and reusable code.
    Signed-off-by: Michael Pacheco <git@michaelpacheco.org>

--- Day-2/rust/src/main.rs
+++ Day-2/rust/src/main.rs
@@ -22,26 +22,30 @@ fn reader(path: &str) -> BufReader<File> {
     let input = File::open(path).unwrap();
     return BufReader::new(input);
 }
-
 fn outcomes() -> HashMap<String, i32> {
-    let mut outcomes = HashMap::new();
-    for line in reader("outcomes.txt").lines() {
-        let line = line.unwrap_or(String::from("//"));
-        // ignore lines that start with //
-        if line.contains("//") || line.is_empty() {
-            continue;
-        }
-        // TODO: Need to figure out if i can simplify this ugly code
-        let outcome = line
-            .split(":")
-            .map(|i| String::from(i))
-            .collect::<Vec<String>>();
-        outcomes.insert(
-            outcome[0].clone(),
-            i32::from_str_radix(outcome[1].clone().as_str(), 10).unwrap_or(0),
-        );
-    }
-    outcomes
+    HashMap::from_iter(
+        _test_map("outcomes.txt")
+            .into_iter()
+            .map(|(outcome, score)| (outcome, i32::from_str_radix(score.as_str(), 10).unwrap_or(0)))
+            .collect::<Vec<(String, i32)>>()
+    )
+    // for (outcome, score) in _test_map("outcomes.txt") {
+    //
+    // }
+}
+
+fn _test_map(path: &str, ) -> impl Iterator<Item = (String, String)> {
+    reader(path)
+        .lines()
+        .into_iter()
+        .map(|line: Result<String, _>| line.unwrap_or(String::from("")))
+        .filter(|line_ref: &String| !(line_ref.contains("//") || line_ref.is_empty()))
+        .map(|line: String| {
+            let mut vec = line.split(":").map(|item| String::from(item));
+            // .collect::<Vec<String, String>>();
+            // TODO: Do some error handling here.
+            (vec.next().unwrap_or_default(), vec.next().unwrap_or_default())
+        })
 }
 
 fn part_1(choices: &HashMap<String, i32>) -> i32 {
@@ -59,30 +63,30 @@ fn part_1(choices: &HashMap<String, i32>) -> i32 {
         if line.is_empty() {
             continue;
         }
-        score += outcomes[&line];
+        score += choices[&line];
     }
     score
 }
 
-fn part_2() {
-    // X means you need to lose, Y means you need to end the round in a draw, and Z means you need to win.
-    let mut choice: HashMap<String, String> = HashMap::new();
-    for line in reader("part2.txt") {
-        // TODO: abstract this code, maybe return a Hashmap in this case.
-        let outcome = line
-            .split(":")
-            .map(|i| String::from(i))
-            .collect::<Vec<String>>();
-        choice.insert(outcome[0].clone(), outcome[1].clone());
-    }
-    for line in reader("../input.txt").lines() {
-        let line = line.unwrap_or(String::from(""));
-        // let outcome = line.split(" ").collect::<Vec<&str>>();
-        if line.is_empty() {
-            continue;
-        }
-        let enemy = line.split(" ").collect::<Vec<&str>>()[0];
-        let p2_choice = choice[&line].as_str();
-        let integrate = format!("{enemy} {p2_choice}");
-    }
-}
+// fn part_2() {
+//     // X means you need to lose, Y means you need to end the round in a draw, and Z means you need to win.
+//     let mut choice: HashMap<String, String> = HashMap::new();
+//     for line in reader("part2.txt") {
+//         // TODO: abstract this code, maybe return a Hashmap in this case.
+//         let outcome = line
+//             .split(":")
+//             .map(|i| String::from(i))
+//             .collect::<Vec<String>>();
+//         choice.insert(outcome[0].clone(), outcome[1].clone());
+//     }
+//     for line in reader("../input.txt").lines() {
+//         let line = line.unwrap_or(String::from(""));
+//         // let outcome = line.split(" ").collect::<Vec<&str>>();
+//         if line.is_empty() {
+//             continue;
+//         }
+//         let enemy = line.split(" ").collect::<Vec<&str>>()[0];
+//         let p2_choice = choice[&line].as_str();
+//         let integrate = format!("{enemy} {p2_choice}");
+//     }
+// }
