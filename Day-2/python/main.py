27f0f9d13d7d1b24cf0ed540bd8aa0ec9727802f
Michael Pacheco <git@michaelpacheco.org>
2022-12-10 18:06:26 -0800

    Part 1 in python done.
    Signed-off-by: Michael Pacheco <git@michaelpacheco.org>

---
+++ Day-2/python/main.py
@@ -0,0 +1,35 @@
+from typing import Iterator
+
+
+def reader(path: str, delimiter: str = "//") -> Iterator[str]:
+    with open(path, "r") as fp:
+        for line in fp:
+            if len(line) == 0 or delimiter in line:
+                continue
+            yield line.strip()
+
+
+def reader_splitter(path: str, splitter: str = ":") -> Iterator[list[str]]:
+    for line in reader(path):
+        yield line.split(splitter)
+
+
+def part_1(day_input: list[str], outcomes: dict[str: int]) -> int:
+    ret = 0
+    for line in day_input:
+        ret += outcomes[line]
+    return ret
+
+
+def main():
+    day_input = [line for line in reader("../input.txt")]
+    outcomes = {line[0]: int(line[1]) for line in reader_splitter("../outcomes.txt")}
+    part_1_score = part_1(day_input, outcomes)
+    print(f"The score for the the player using the guide is {part_1_score}")
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    main()
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
