a32d027c4d58f8f7a19e39e200599ea8f322a89e
Michael Pacheco <git@michaelpacheco.org>
2022-12-10 18:15:59 -0800

    Part 2 done in python.
    Signed-off-by: Michael Pacheco <git@michaelpacheco.org>

--- Day-2/python/main.py
+++ Day-2/python/main.py
@@ -21,11 +21,23 @@ def part_1(day_input: list[str], outcomes: dict[str: int]) -> int:
     return ret
 
 
+def part_2(day_input: list[str], outcomes: dict[str: int], conditions: dict[str: str]) -> int:
+    ret = 0
+    for line in day_input:
+        enemy = line.split(" ")[0]
+        choice = conditions[line]
+        ret += outcomes[f'{enemy} {choice}']
+    return ret
+
+
 def main():
     day_input = [line for line in reader("../input.txt")]
     outcomes = {line[0]: int(line[1]) for line in reader_splitter("../outcomes.txt")}
     part_1_score = part_1(day_input, outcomes)
     print(f"The score for the the player using the guide is {part_1_score}")
+    conditions = {line[0]: line[1] for line in reader_splitter("../part2.txt")}
+    part_2_score = part_2(day_input, outcomes, conditions)
+    print(f"The score for the player with part 2's conditions in mind is {part_2_score}")
 
 
 # Press the green button in the gutter to run the script.
