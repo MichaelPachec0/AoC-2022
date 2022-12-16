f42ba72935f0fde8086f339ecf4acf34a01fc401
Michael Pacheco <git@michaelpacheco.org>
2022-12-15 19:27:17 -0800

    Day-1:part-1:elixir: Finish Part 2.
    Signed-off-by: Michael Pacheco <git@michaelpacheco.org>

--- Day-1/elixir/CalCalculator.ex
+++ Day-1/elixir/CalCalculator.ex
@@ -1,13 +1,14 @@
 defmodule CalCalculator do
-  def calculate(input) do
+  defp calculate_helper(input) do
     input
     |> String.splitter("\n\n", trim: true)
-    |> Stream.map(fn elf ->
-      elf
+    |> Stream.map(fn elf -> 
+      elf 
       |> String.splitter("\n")
-      |> Stream.map(fn cal -> cal |> String.to_integer() end)
-      |> Enum.sum()
+      |> Stream.map(fn cal -> cal |> String.to_integer end)
+      |> Enum.sum
     end)
-    |> Enum.max()
   end
+  def calculate_part1(input), do: input |> calculate_helper |> Enum.max
+  def calculate_part2(input), do: input |> calculate_helper |> Enum.sort |> Enum.reverse |> Enum.take(3) |> Enum.sum
 end
