dd38523b5d62bcd2b28dd56d2c0844217e6bbc9c
Michael Pacheco <git@michaelpacheco.org>
2022-12-16 23:58:48 -0800

    Day-1:misc:rust: Added declarative soltion
    This was an interesting way of finishing the problem, since the input is all in memory (as opposed to what i imagine buf lines handling some of that behind the scenes). This means that i only need to send references to everything, which means that the memory cost is not constant as with the imperative approach, this will now be a O(n) space solution. But it is a *much* faster solution because there are no complicated buffering handled by rust std lib.
    
    * Also applied the more liberal use of the ? operator in my code. which greatly simplified it, the python code in comparison looks incredibly wordy, although not as simple as the elixir code, although in in defense of rust, there is no timing code embedded in it, and its is more duck typed (the input is of course of string) which makes reading the rest of the code harder since the elixir solution to get to its compactness is nesting iterators.
    
    * Also the rust code is pretty fast, i have gotten as far down as ~67µs a the declarative run.
    
    Signed-off-by: Michael Pacheco <git@michaelpacheco.org>

--- Day-1/rust/src/main.rs
+++ Day-1/rust/src/main.rs
@@ -1,3 +1,5 @@
+use itertools::Itertools;
+use std::fs;
 use std::fs::File;
 use std::io::{BufRead, BufReader};
 use std::time::{Duration, Instant};
@@ -11,6 +13,40 @@ fn main() {
 
     let duration: Duration = start.elapsed();
     println!("Time elapsed is: {duration:?}");
+
+    println!("How about a more declarative approach?");
+    let duration: Duration = start.elapsed();
+    declarative().expect("Oh no, he's dead jim");
+    let duration = start.elapsed() - duration;
+    println!("Time elapsed now is {duration:?}")
+}
+
+fn declarative() -> Result<(), Box<dyn std::error::Error>> {
+    let input = reader("../input.txt")?;
+    let x = declarative_helper(&input)?.sorted().collect::<Vec<i32>>();
+    println!(
+        "The answer for part 1 is {}",
+        x.last().ok_or("Whelp we dont have this value")?
+    );
+    println!(
+        "The answer for part 2 is {}",
+        x[x.len() - 3..].iter().sum::<i32>()
+    );
+    Ok(())
+}
+
+fn declarative_helper(
+    input: &str,
+) -> Result<impl Iterator<Item = i32> + '_, Box<dyn std::error::Error>> {
+    Ok(input.split("\n\n").map(|elf| {
+        elf.split("\n")
+            .map(|cal| i32::from_str_radix(cal, 10).unwrap_or_default())
+            .sum()
+    }))
+}
+
+fn reader(path: &str) -> Result<String, Box<dyn std::error::Error>> {
+    Ok(fs::read_to_string(path)?.parse()?)
 }
 
 fn imperative() -> Result<(), Box<dyn std::error::Error>> {
