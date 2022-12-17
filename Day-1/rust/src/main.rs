5881a0f4fc1a80a774453ea936c68b1a2c76237d
Michael Pacheco <git@michaelpacheco.org>
2022-12-16 23:43:34 -0800

    Day-1:misc:rust: Redid problem.
    * Wanted to create a more imperative solution (whereas the initial solution was done with a combination of both, seeing as there was a for_each before).
    
    * Redid the line handler function so that now it mutates the cur_cal variable in place and only returns a true/ false, simplified the code so that it can be dropped in a if statement
    
    * And finally added the solution for part 2, since this was only done in python which was something i overlooked.
    
    Signed-off-by: Michael Pacheco <git@michaelpacheco.org>

--- Day-1/rust/src/main.rs
+++ Day-1/rust/src/main.rs
@@ -4,33 +4,49 @@ use std::time::{Duration, Instant};
 
 // Answer for part 1 is 71124. For part 2 its 204639
 
-fn main() -> Result<(), Box<dyn std::error::Error>> {
+fn main() {
     let start: Instant = Instant::now();
-    let path = "../input.txt";
-    let input = File::open(path)?;
+
+    imperative().expect("Whoops it failed");
+
+    let duration: Duration = start.elapsed();
+    println!("Time elapsed is: {duration:?}");
+}
+
+fn imperative() -> Result<(), Box<dyn std::error::Error>> {
+    let path: &str = "../input.txt";
+    let input: File = File::open(path)?;
 
     let buf = BufReader::new(input);
 
     let mut cur_cal: i32 = 0;
-    let mut max_cal: i32 = 0;
+    let mut top: [i32; 3] = [0; 3];
 
-
-    buf.lines().for_each(|line| {
-        max_cal = line_handler(line.unwrap(), &mut cur_cal, max_cal)
-    });
-    println!("The answer for Day 1 part 1 is {max_cal}");
-    let duration: Duration = start.elapsed();
-    println!("Time elapsed is: {duration:?}");
+    for line in buf.lines() {
+        if line_handler(line?, &mut cur_cal) {
+            for (i, cal) in top.iter_mut().enumerate() {
+                if cur_cal > *cal {
+                    top[i] = cur_cal;
+                    break;
+                }
+            }
+            cur_cal = 0;
+        }
+    }
+    let mut sum = 0;
+    for &cal in &top {
+        sum += cal;
+    }
+    println!("The answer for part 1 is {}", top[0]);
+    println!("The answer for part 2 is {}", sum);
     Ok(())
 }
 
-fn line_handler(line: String, cur: &mut i32, max: i32) -> i32 {
+fn line_handler(line: String, cur: &mut i32) -> bool {
     if line.len() != 0 {
         *cur += line.parse::<i32>().unwrap_or(0);
-        max
+        false
     } else {
-        let tmp: i32 = *cur;
-        *cur = 0;
-        (tmp > max) as i32 * tmp + (tmp < max) as i32 * max
+        true
     }
 }
